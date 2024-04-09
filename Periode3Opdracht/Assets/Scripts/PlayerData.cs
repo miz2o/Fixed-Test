33f831806070fb2ee326ccc261c3d42b7aa46a5a
bubblexis <mendrika05@gmail.com>
2024-04-09 12:28:54 +0200

    njnhebBJKH3UQ

--- Periode3Opdracht/Assets/Scripts/PlayerData.cs
+++
@@ -1,8 +0,0 @@
-using System;
-
-[Serializable]
-public class PlayerData
-{
-    // Add variables to store necessary game information (player position, health, etc.)
-    // Example: public Vector3 playerPosition;
-}
