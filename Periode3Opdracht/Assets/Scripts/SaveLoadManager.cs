bd65fa886e165def19dcc6f1a482be23996a8ae2
bubblexis <mendrika05@gmail.com>
2024-04-11 15:22:08 +0200

    kkqjquh2f

--- Periode3Opdracht/Assets/Scripts/SaveLoadManager.cs
+++ Periode3Opdracht/Assets/Scripts/SaveLoadManager.cs
@@ -1,28 +1,45 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class SaveLoadManager : MonoBehaviour
 {
+    public TMP_InputField inputField;
+
+
 
     // Function to save the game
     public void SaveGame()
     {
+        if (inputField == null)
+        {
+            Debug.LogError("inputField is null!");
+            return;
+        }
 
         // Save player position
         PlayerPrefs.SetFloat("PlayerX", transform.position.x);
         PlayerPrefs.SetFloat("PlayerY", transform.position.y);
         PlayerPrefs.SetFloat("PlayerZ", transform.position.z);
 
-
+        PlayerPrefs.SetString("Input", inputField.text);
 
         // Save PlayerPrefs to disk
         PlayerPrefs.Save();
     }
 
+
+
     // Function to load the game
     public void LoadGame()
     {
+        if (inputField == null)
+        {
+            Debug.LogError("inputField is null!");
+            return;
+        }
 
         // Load player position
         float playerX = PlayerPrefs.GetFloat("PlayerX");
@@ -30,5 +47,8 @@ public class SaveLoadManager : MonoBehaviour
         float playerZ = PlayerPrefs.GetFloat("PlayerZ");
         transform.position = new Vector3(playerX, playerY, playerZ);
 
+
+
+        inputField.text = PlayerPrefs.GetString("Input");
     }
 }
